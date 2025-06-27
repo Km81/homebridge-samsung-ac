2f69b6cf978cfc2ac9c838c2c7d845f7d4b93527
Km81 <km81.ryu@icloud.com>
2025-06-27 17:19:30 +0900

    Update Server8889.py

--- Server8889.py
+++ Server8889.py
@@ -44,7 +44,7 @@ def main():
     port = 8889
     print('Listening on localhost:%s' % port)
     server = HTTPServer(('', port), RequestHandler)
-    server.socket = ssl.wrap_socket(server.socket, certfile='/var/lib/homebridge/node_modules/homebridge-samsung-ac/ac14k_m.pem', server_side=True)
+    server.socket = ssl.wrap_socket(server.socket, certfile='/usr/local/lib/node_modules/homebridge-samsung-ac/ac14k_m.pem', server_side=True)
     server.serve_forever()
 
 
