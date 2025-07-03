893803ad7c0851c0fe34bc626316cae87dc1a63c
Km81 <km81.ryu@icloud.com>
2025-07-03 19:27:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Samsung Air Conditioner Homebridge Plugin
-// Version 2.0.8 (Final fix for UI settings restoration)
+// Version 2.0.9 (Final fix for UI settings restoration)
 'use strict';
 
 const tls = require('tls');
